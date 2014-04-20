7a2cf2b157c0dc55f5741028462e24e2e3b299eb
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-04-20 23:46:42 +0530

    Huffman decoding added

--- knit/huffman.py
+++ knit/huffman.py
@@ -20,7 +20,7 @@ class Node:
 		return not other<self
 
 def getHuffmanCodes(q):
-	codes = []
+	codes = {}
 	path = []
 	_getHuffmanCodes(q, path, codes)
 	return codes
@@ -28,19 +28,20 @@ def getHuffmanCodes(q):
 def _getHuffmanCodes(q, path, codes):
 	if q is not None:
 		if q.left is None and q.right is None:
-			codes.append((q.c, path))
+			codes[q.c] = path
 		_getHuffmanCodes(q.left, path + [False], codes)
 		_getHuffmanCodes(q.right, path + [True], codes)
 
-def encode(string):
+def getCounts(string):
 	count = {}
 	for i in string:
 		if i in count:
 			count[i] += 1
 		else:
 			count[i] = 1
-	del string
+	return count
 
+def getHuffmanTree(count):
 	n = len(count)
 	q = []
 	for i in count:
@@ -56,7 +57,29 @@ def encode(string):
 		z.right = heapq.heappop(q)
 		z.freq = z.left.freq + z.right.freq
 		heapq.heappush(q,z)
-	return getHuffmanCodes(q[0])
+	return q[0]
+
+def encode(string):
+	counts = getCounts(string)
+	codes = getHuffmanCodes(getHuffmanTree(counts))
+	bitarray = []
+	for i in string:
+		bitarray = bitarray + codes[i]
+	return (counts, bitarray)
+
+def decode((counts, bitarray)):
+	q = getHuffmanTree(counts)
+	p = q
+	string = ''
+	for i in bitarray:
+		if i:
+			p = p.right
+		else:
+			p = p.left
+		if p.left is None and p.right is None:
+			string += p.c
+			p = q
+	return string
 
 if __name__ == "__main__":
-	print encode("chaitanya")
+	print decode(encode("chaitanya"))
